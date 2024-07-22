6ae2090ee8475feb43e89a31fe6be45f5457a87a
kamal banoudhia <kamalb@abpnetwork.com>
2024-07-22 13:57:02 +0000

    update

--- gs/conn.php
+++ gs/conn.php
@@ -1,8 +1,8 @@
 <?php
-$sname = "127.0.0.1";
+$sname = "video-cms.cqnwvxrxwjzo.ap-south-1.rds.amazonaws.com";
 $uname = "root";
-$pwd = "12345678";
-$dbname = "tool";
+$pwd = "rbKT$9WC2+M3E3C";
+$dbname = "data_project";
 $conn = mysqli_connect($sname, $uname, $pwd, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
